fdfa9e411c6575c1057cd0ae537ed5d2d2a4643f
Jawahirul Fanani <kyuraz029@gmail.com>
2023-07-07 11:24:04 +0700

    Pre Final update eda and visualization

--- streamlit/Dataset.py
+++ streamlit/Dataset.py
@@ -2,14 +2,14 @@ import streamlit as st
 import pandas as pd 
 import plotly.express as px
 
-df = pd.read_csv('../clean_data/salary-data.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/jawahirulfn/UAS-Visualisasi_Data/main/dataset/Salary_Data.csv')
 
 st.title('Dataset Introduction')
 st.divider()
 
 # DATASET
 st.subheader('Dataset')
-st.markdown("Data is obtained from a variety of sources, including surveys, job sites, and other publicly available sources. The dataset includes five variables: age, years_of_experience, job, education_level and salary. We have cleaned this dataset. Our clean data process includes renaming several column names, handle duplicated data, handle null values, & handle outliers")
+st.markdown("Data is obtained from a variety of sources, including surveys, job sites, and other publicly available sources. The dataset includes five variables: Age, Gender, Education Level, Job Title, Years of Experience and Salary. ")
 st.write("Dataset : ""[salary data](https://www.kaggle.com/datasets/mohithsairamreddy/salary-data)")
 st.write("Project : ""[Github](https://github.com/jawahirulfn/UAS-Visualisasi_Data)")
 st.write(df)
